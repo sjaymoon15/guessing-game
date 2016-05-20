c2a938f0f7d341c4737af23e757043d8b65cdd90
Seungjae Moon <sjaymoon15@gmail.com>
2016-05-19 23:23:29 -0400

    Add lowerOrHiger() guessMessage()

--- guessingGame.js
+++ guessingGame.js
@@ -3,10 +3,11 @@
 	// try to elminate these global variables in your project, these are here just to start.
 
 	var playersGuess,
-	    winningNumber;
+	    winningNumber,
+	    guessArray = [];
 
 	winningNumber = generateWinningNumber();
-	console.log(winningNumber);
+	console.log("win number " + winningNumber);
 	playersGuessSubmission();
 	
 	
@@ -26,7 +27,8 @@
 		$("#submit-button").on("click", function(){
 			playersGuess = parseInt($("#input-here").val());
 			$("#input-here").val("");
-			console.log(playersGuess);
+			console.log("playersGuess " + playersGuess);
+
 			checkGuess();
 		})
 	}
@@ -35,19 +37,48 @@
 
 	function lowerOrHigher(){
 		// add code here
+		if(playersGuess > winningNumber){
+			return "Your guess is higher ";
+		}else if(playersGuess < winningNumber){
+			return "Your guess is lower "; 
+		}
 	}
 
 	// Check if the Player's Guess is the winning number 
 
 	function checkGuess(){
 		// add code here
-		if(playersGuess == winningNumber){
+		
+		if(playersGuess === winningNumber){
 			$("#status").html("You Win!");
-		}else{
-			$("#status").html("Try Again!");
-		}
-	}
 
+		}else{
+			if(guessArray.indexOf(playersGuess) !== -1){
+				$("#status").html("You already entered the number!");
+			}else{
+				guessArray.push(playersGuess);
+				console.log(guessArray);
+				var guessLeft = parseInt($("#guess-left").html());
+				console.log(guessLeft + " guess left");
+				guessLeft -= 1;
+				console.log(guessLeft);
+				$("#guess-left").html(guessLeft);
+				$("#status").html("Try Again!");
+				$("#message").html(guessMessage());
+			}
+		}
+
+	}
+	function guessMessage(){
+		var message = "";
+		var distance = Math.abs(playersGuess - winningNumber);
+		if(distance < 10){
+			message = "and within 10 digits of the winning number";
+		}else if(distance >= 10 && distance < 20){
+			message = "and within 20 digits of the winning number"
+		}
+		return lowerOrHigher()+ message; 
+	}
 	// Create a provide hint button that provides additional clues to the "Player"
 
 	function provideHint(){
